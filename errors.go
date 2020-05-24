8707513eb26747ce44f7d53fb35711a4aa71b9a9
Steve Partridge <srpartridge@gmail.com>
2020-05-24 15:08:53 -0700

    Clean up error messages

--- errors.go
+++ errors.go
@@ -14,15 +14,19 @@ var (
 
 	// ErrCreateUserIDPresent user missing ID error
 	ErrCreateUserIDPresent = errors.New("blog.users.user.create.user_id.present")
+
 	// ErrCreateUserNameMissing user missing Name error
 	ErrCreateUserNameMissing = errors.New("blog.users.user.create.missing.name")
 
 	// ErrUpdateUserIDMissing user missing ID error
 	ErrUpdateUserIDMissing = errors.New("blog.users.user.update.missing.id")
+
 	// ErrUpdateUserInvalidID user invalid ID error
 	ErrUpdateUserInvalidID = errors.New("blog.users.user.update.invalid.id")
+
 	// ErrUpdateUserNameMissing user missing Name error
 	ErrUpdateUserNameMissing = errors.New("blog.users.user.update.missing.name")
+
 	// ErrUpdateUserUpdatedByMissing user missing UpdatedByID error
 	ErrUpdateUserUpdatedByMissing = errors.New("blog.users.user.update.missing.updated_by")
 
@@ -31,52 +35,58 @@ var (
 	// ErrDeleteUserMissingDeletedByID user missing deletedBy error
 	ErrDeleteUserMissingDeletedByID = errors.New("blog.users.user.delete.missing.deleted_by")
 
-	// ErrGetUserByAPIKeyMissingAPIKey
+	// ErrGetUserByAPIKeyMissingAPIKey error message
 	ErrGetUserByAPIKeyMissingAPIKey = errors.New("blog.users.user.get_by_api_key.missing.api_key")
 
-	// ErrGetUserIDByAPIKeyMissingAPIKey
+	// ErrGetUserIDByAPIKeyMissingAPIKey error message
 	ErrGetUserIDByAPIKeyMissingAPIKey = errors.New("blog.users.user.get_user_id_by_api_key.missing.api_key")
 
-	// ErrGetAPIKeyForUserIDMissingUserID
+	// ErrGetAPIKeyForUserIDMissingUserID error message
 	ErrGetAPIKeyForUserIDMissingUserID = errors.New("blog.users.user.get_api_key_for_user_id.missing.user_id")
 
-	// ErrGetUserNotFound error
+	// ErrGetUserNotFound  error message
 	ErrGetUserNotFound = errors.New("blog.users.user.not_found")
 
 	// Posts
 	//
 
-	// ErrGetPostsByIDsMissingMinimumIDs
+	// ErrGetPostsByIDsMissingMinimumIDs error message
 	ErrGetPostsByIDsMissingMinimumIDs = errors.New("blog.posts.post.get_by_ids.missing.minimum_one_id")
 
-	// ErrCreatePostIDPresent
+	// ErrCreatePostIDPresent error message
 	ErrCreatePostIDPresent = errors.New("blog.posts.create.post_id.present")
 
-	// ErrCreatePostTitleMissing
+	// ErrCreatePostTitleMissing error message
 	ErrCreatePostTitleMissing = errors.New("blog.posts.create.missing.title")
 
-	// ErrCreatePostContentMissing
+	// ErrCreatePostContentMissing error message
 	ErrCreatePostContentMissing = errors.New("blog.posts.create.missing.content")
 
-	// ErrUpdatePostIDMissing
+	// ErrUpdatePostIDMissing error message
 	ErrUpdatePostIDMissing = errors.New("blog.posts.update.missing.id")
 
-	// ErrUpdatePostTitleMissing
+	// ErrUpdatePostTitleMissing error message
 	ErrUpdatePostTitleMissing = errors.New("blog.posts.update.missing.title")
 
-	// ErrUpdatePostContentMissing
+	// ErrUpdatePostContentMissing error message
 	ErrUpdatePostContentMissing = errors.New("blog.posts.update.missing.content")
 
-	// ErrUpdatePostUpdatedByMissing
+	// ErrUpdatePostUpdatedByMissing error message
 	ErrUpdatePostUpdatedByMissing = errors.New("blog.posts.update.missing.updated_by")
 
-	// ErrDeletePostMissingID
+	// ErrUpdatePostInvalidUser error message
+	ErrUpdatePostInvalidUser = errors.New("blog.posts.update.invalid.updated_by")
+
+	// ErrDeletePostInvalidUser error message
+	ErrDeletePostInvalidUser = errors.New("blog.posts.post.delete.user_not_permitted")
+
+	// ErrDeletePostMissingID error message
 	ErrDeletePostMissingID = errors.New("blog.posts.delete.missing.id")
 
-	// ErrDeletePostMissingDeletedByID
+	// ErrDeletePostMissingDeletedByID error message
 	ErrDeletePostMissingDeletedByID = errors.New("blog.posts.delete.missing.deleted_by_id")
 
-	// ErrGetPostNotFound error
+	// ErrGetPostNotFound error message
 	ErrGetPostNotFound = errors.New("blog.posts.post.not_found")
 )
 
